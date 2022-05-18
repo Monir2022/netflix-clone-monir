c8244b7ddc48aead2d36412d93fb2c0bd0216af1
Monir <akhandbuet@gmail.com>
2022-05-18 19:19:43 +0200

    css modification

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,6 @@
 // NPM Packages
 import { useState } from "react";
 import { Link, useHistory } from "react-router-dom";
-
 // Project files
 import InputField from "components/InputField";
 import fields from "data/fields-login.json";
@@ -9,29 +8,24 @@ import { signIn } from "scripts/authentification";
 import { getDocument } from "scripts/fireStore";
 import { useAuth } from "state/AuthProvider";
 import Logo from "assets/logo.png";
-
 export default function Login() {
   // Global state
   const { setUser, setIsLogged } = useAuth();
   const history = useHistory();
-
   // Local state
   const [form, setForm] = useState({});
   const [errorMassage, setErrorMessage] = useState("");
-
   // Methods
   function onChange(key, value) {
     const field = { [key]: value };
     setForm({ ...form, ...field });
   }
-
   async function onSubmit(event) {
     event.preventDefault();
     setErrorMessage("");
     const account = await signIn(form.email, form.password);
     account.isLogged ? onSuccess(account.payload) : onFailure(account.payload);
   }
-
   async function onSuccess(uid) {
     const document = await getDocument("users", uid);
     setUser(document);
@@ -39,11 +33,9 @@ export default function Login() {
     localStorage.setItem("uid", uid);
     history.push("/");
   }
-
   function onFailure(message) {
     setErrorMessage(message);
   }
-
   // Components
   const InputFields = fields.map((item) => (
     <InputField
@@ -53,7 +45,6 @@ export default function Login() {
       onChange={onChange}
     />
   ));
-
   return (
     <div id="signin-page">
       <header>
@@ -66,9 +57,18 @@ export default function Login() {
             {InputFields}
             <p>{errorMassage}</p>
             <button>Sign In</button>
-            <Link to= "/recover-password">Need help?</Link>
-            <small>New to Netflix?</small>
-            <Link to="/signup">Sign up now</Link>
+            <input type="checkbox" />            
+            <small>Remember me</small>
+            <Link className="need-help" to="/recover-password">
+              <small>Need help?</small>
+            </Link>                         
+            <div className="sign-bottom">
+              <small>New to Netflix?</small>
+              <Link to="/signup">Sign up now</Link>
+            </div>
+            <small>
+              This page is protected by Google to ensure that you are not a bot
+            </small>
           </form>
         </div>
       </div>
