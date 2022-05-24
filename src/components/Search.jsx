9fff7f4c19e6ff300e62551eaa6374f6bb38d196
Monir <akhandbuet@gmail.com>
2022-05-24 22:50:39 +0200

    Series has added

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,30 +1,25 @@
-
+import { useState } from "react";
 //Project files
 import { useSearch } from "state/SearchProvider";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
-import { useState } from "react/cjs/react.production.min";
 
 export default function Search() {
   //Global state
   const { search } = useSearch();
   const { onChange, setSearch } = useSearch();
+  const { find } = useSearch();
 
+  const [message, setMessage] = useState(" ");
 
+  const icon = <FontAwesomeIcon icon={faMagnifyingGlass} />;
   
-
-  
-  
-  
-
-  const icon = <FontAwesomeIcon icon={faMagnifyingGlass} />
 
   return (
     <div id="search">
       <div className="icon">{icon}</div>
       <input value={search} onChange={onChange} placeholder="Search" />
       <button onClick={() => setSearch(" ")}>X</button>
-      
     </div>
   );
 }
